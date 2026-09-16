b6464dad3e792c50a0aa74219b4d31d50e8d08c6
ASPAK0868 <ashpakchadachan1@gmail.com>
2026-09-16 12:18:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 import express from "express";
 import { WebSocketServer } from "ws";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("."));
+app.use(express.static("Public"));
 
 const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
